86d0a422e7dd173eed524b7e0a5b54e6e89ed736
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-05-07 15:34:23 +0200

    Update MacchinettaView.java

---
+++ View/MacchinettaView.java
@@ -0,0 +1,22 @@
+package View;
+import Model.Bevanda;
+
+public class MacchinettaView {
+    public void visualizzaStorico(String storicoOrdini) {
+        System.out.println("Storico ordini: ");
+        System.out.println(storicoOrdini);
+    }
+    
+    
+    public void visualizzaCosto() {
+        System.out.println("MacchinettaView: visualizzaCosto");
+    }
+    
+    public void visualizzaDescrizione() {
+        System.out.println("MacchinettaView: visualizzaDescrizione");
+    }
+    
+    public void visualizzaStorico() {
+        System.out.println("MacchinettaView: visualizzaStorico");
+    }
+}
