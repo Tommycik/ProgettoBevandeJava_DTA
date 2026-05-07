563bc1fde7e2c7e53c892557213b3d89a3a11676
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-05-07 18:29:58 +0200

    Update StrategyStudente.java

--- Controller/StrategyStudente.java
+++ Controller/StrategyStudente.java
@@ -5,8 +5,8 @@ public class StrategyStudente implements StrategySconti {
 
     @Override
     public double processaSconti(Bevanda bevanda) {
-        double costo = bevanda.getCosto();
-        return costo*0.8;
+        double costo = bevanda.getCosto() * 0.80;
+        return Math.round(costo * 100.0) / 100.0;
     }
     
 }
