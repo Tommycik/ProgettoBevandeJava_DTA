4dbc7a41691e73a4b1458ccd85356df5be277f9b
Danilo Mosca <danilo.mosca85@gmail.com>
2026-05-07 16:27:57 +0200

    added controller in MacchinettaController.java
    added controller in MacchinettaController.java

---
+++ Controller/MacchinettaController.java
@@ -0,0 +1,114 @@
+package Controller;
+
+import Model.*;
+import View.MacchinettaView;
+
+public class MacchinettaController {
+
+    private BarFacade facade;
+    private MacchinettaView view;
+
+    private Bevanda bevandaCorrente;
+
+    public MacchinettaController() {
+        this.facade = new BarFacade();
+        this.view = new MacchinettaView();
+    }
+
+    // MENU
+    public void visualizzaMenu() {
+        view.visualizzaMenu();
+    }
+
+    public void visualizzaBevande() {
+        view.visualizzaBevande();
+    }
+
+    public void visualizzaIngredientiExtra() {
+        view.visualizzaIngredientiExtra();
+    }
+
+    // BEVANDA
+    public void creaNuovaBevanda(int scelta) {
+
+        switch (scelta) {
+            case 1:
+                bevandaCorrente = facade.creaCaffe();
+                break;
+            case 2:
+                bevandaCorrente = facade.creaTe();
+                break;
+            case 3:
+                bevandaCorrente = facade.creaCioccolata();
+                break;
+            default:
+                System.out.println("Scelta non valida");
+                return;
+        }
+
+        view.visualizzaBevandaCorrente(bevandaCorrente);
+    }
+
+    // EXTRA
+    public void aggiungiIngredienteExtra(int scelta) {
+
+        if (bevandaCorrente == null) {
+            System.out.println("Devi prima creare una bevanda!");
+            return;
+        }
+
+        switch (scelta) {
+
+            case 1:
+                bevandaCorrente = new DecoratorLatte(bevandaCorrente);
+                break;
+
+            case 2:
+                bevandaCorrente = new DecoratorCannella(bevandaCorrente);
+                break;
+
+            case 3:
+                bevandaCorrente = new DecoratorCacao(bevandaCorrente);
+                break;
+
+            case 4:
+                bevandaCorrente = new DecoratorPanna(bevandaCorrente);
+                break;
+
+            case 5:
+                bevandaCorrente = new DecoratorZucchero(bevandaCorrente);
+                break;
+
+            default:
+                System.out.println("Ingrediente non valido");
+                return;
+        }
+
+        view.visualizzaBevandaCorrente(bevandaCorrente);
+    }
+
+    // ORDINE
+    public void confermaOrdine() {
+
+        if (bevandaCorrente == null) {
+            System.out.println("Nessuna bevanda da confermare");
+            return;
+        }
+
+        facade.archiviaOrdine(bevandaCorrente);
+
+        view.confermaOrdine(bevandaCorrente);
+
+        bevandaCorrente = null;
+    }
+
+    // STORICO
+    public void visualizzaStorico() {
+        view.visualizzaStorico(facade.stampaStorico());
+    }
+
+    // GETTER (opzionale debug)
+    public Bevanda getBevandaCorrente() {
+        return bevandaCorrente;
+    }
+}
