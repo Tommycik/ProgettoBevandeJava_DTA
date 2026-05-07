30a034c2a63364001686e0ad50dc7b9f5288d9f3
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-05-07 15:44:00 +0200

    Update MacchinettaView.java

--- View/MacchinettaView.java
+++ View/MacchinettaView.java
@@ -8,10 +8,29 @@ public class MacchinettaView {
         System.out.println(storicoOrdini);
     }
     
-    //visualizza descrizione bevanda
-    public void StampaBevanda(Bevanda bevanda) {
-        System.out.println("Bevanda scelta:");
+    //helper stampa bevanda
+    private void stampaBevanda(Bevanda bevanda) {
         System.out.println(bevanda.getDescrizione());
         System.out.println(bevanda.getCosto());
     }
+    //conferma ordine
+    public void confermaOrdine(Bevanda bevanda) {
+        System.out.println("Ordine confermato");
+        stampaBevanda(bevanda);
+    }
+    //bevanda corrente
+    public void visualizzaBevandaCorrente(Bevanda bevanda) {
+        System.out.println("Bevanda corrente");
+        stampaBevanda(bevanda);
+    }
+    //Stampa menu azioni
+    public void visualizzaMenu() {
+        System.out.println("Menu azioni:");
+        System.out.println("1. Crea nuova bevanda");
+        System.out.println("2. Aggiungi ingrediente ");
+        System.out.println("3. Conferma ordine");
+        System.out.println("3. Visualizza bevanda corrente");
+        System.out.println("4. Esci");
+    }
+
 }
